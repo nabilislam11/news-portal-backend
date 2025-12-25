a44237886a22b8eea8dbe1c53095b507d766fd0f
sanvir-islam <sanvirofficials@gmail.com>
2025-12-26 04:08:25 +0600

    updated email login for production

--- middleware/rateLimiter.ts
+++ middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 200,
+  max: 500,
   message: {
     success: false,
     message: "Too many requests from this IP, please try again after 15 minutes",
