9dd9381849f57a5f185747cdf00b1360f7b7dfb4
sanvir-islam <sanvirofficials@gmail.com>
2025-12-31 15:07:44 +0600

    hello

--- dist/routes/api/navMenu.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const navMenuController_1 = require("../../controllers/navMenuController");
-const authMiddleware_1 = require("../../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.route("/").get(navMenuController_1.getNavMenu).put(authMiddleware_1.verifyAuthToken, navMenuController_1.updateNavMenu);
-exports.default = router;
-//# sourceMappingURL=navMenu.js.map
--- dist/utils/createError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createError = createError;
-function createError(message, statusCode = 500) {
-    const error = new Error(message);
-    error.statusCode = statusCode;
-    return error;
-}
-//# sourceMappingURL=createError.js.map
